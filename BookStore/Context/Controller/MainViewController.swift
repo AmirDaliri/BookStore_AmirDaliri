1aac93de82a75a7afd195fd25345a4614a1bdad2
EmirDaliri <naser.daliribejindi@consultant.turkcell.com.tr>
2020-11-18 18:21:26 +0300

    fetch books in main controller

--- BookStore/Context/Controller/MainViewController.swift
+++ BookStore/Context/Controller/MainViewController.swift
@@ -9,14 +9,44 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // MARK: - Properties
+    private var mainViewModel = MainViewModel()
+    private var disposal = Disposal()
+    
     // MARK: - Lifecycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // I'm Here...
-        MainRequest.shared.getBooks(index: 1) { (err, books) in
-            print(books)
+        bindUI()
+        mainViewModel.getBooks(index: 0)
+    }
+
+    // MARK: - Private Method
+    private func bindUI() {
+        mainViewModel.state.observe { [weak self] (state) in
+            guard let self = self else { return }
+            switch state {
+            case .success:
+                self.updateUI()
+            case .error(let err):
+                self.handleAlertView(err: err)
+            default:
+                break
+            }
+        }.add(to: &disposal)
+    }
+    
+    private func updateUI() {
+        // TODO:  load UI
+    }
+    
+    private func handleAlertView(err: BackendError?) {
+        if let error = err {
+            AlertBuilder().message(error.localizedDescription)
+                .addCancelAction(title: "OK")
+            .show(in: self)
         }
     }
 }
